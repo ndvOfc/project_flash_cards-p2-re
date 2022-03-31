cbe45b5f3b48621bbd61af18e73d110b193dca14
vasily <moonalty@gmail.com>
2022-03-31 13:56:53 +0300

    release 0.2 routes

--- config/config.js
+++ config/config.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const hbs = require("hbs");
 const path = require("path");
+const session = require("express-session");
+const sessionFileStore = require("session-file-store");
+const cookieParser = require("cookie-parser");
+
+const FileStore = sessionFileStore(session);
 
 const config = (app) => {
   app.set("view engine", "hbs");
@@ -10,6 +15,17 @@ const config = (app) => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(express.static(path.join(process.env.PWD, "public")));
+  app.use(cookieParser());
+  app.use(
+    session({
+      key: app.get("session cookie name"),
+      secret: "secret",
+      store: new FileStore({ secret: "secret" }),
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 10e3 * 60, httpOnly: true },
+    })
+  );
 };
 
 module.exports = config;
