e26ac772086ae2993bdc1c69017b57a66c75f3bd
vasily <moonalty@gmail.com>
2022-03-31 17:47:05 +0300

    release 0.3 database

---
+++ db/models/question.js
@@ -0,0 +1,36 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Question extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Topic }) {
+      Question.hasOne(Topic, { foreignKey: "topic_id" });
+      // define association here
+    }
+  }
+  Question.init(
+    {
+      question: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      answer: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+      },
+      topic_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Question",
+    }
+  );
+  return Question;
+};
---
+++ db/models/topic.js
@@ -0,0 +1,29 @@
+"use strict";
+const { Model } = require("sequelize");
+const question = require("./question");
+module.exports = (sequelize, DataTypes) => {
+  class Topic extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate({ Question }) {
+      Topic.hasMany(Question, { foreignKey: "topic_id" });
+      // define association here
+    }
+  }
+  Topic.init(
+    {
+      name: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Topic",
+    }
+  );
+  return Topic;
+};
